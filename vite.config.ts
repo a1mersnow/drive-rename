3abcfc868ea128f6e3c2880d426623c134a3c81a
svcvit <svcvit@gmail.com>
2023-11-25 11:06:51 +0800

    增加alipan域名

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
       userscript: {
         grant: 'none',
         match: [
-          'https://www.aliyundrive.com/drive/*',
+          'https://www.aliyundrive.com/drive/*','https://www.alipan.com/drive/*'
         ],
         license: 'GPL',
         name: '阿里云盘批量重命名',
